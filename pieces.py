394a591817396e28ffcdaf4f8213a74ce850667e
matthewrb <reedbrownm@gmail.com>
2024-09-05 21:51:31 -0400

    Change to use new Move class to represent all spaces travelled by piece

--- pieces.py
+++ pieces.py
@@ -1,3 +1,11 @@
+class Move:
+    """Represents the potential movement of a piece from one space to another, storing the coordinates of all spaces through which the piece must travel"""
+    def __init__(self, spaces: list[tuple[int,int]]) -> None:
+        self.spaces = spaces
+    
+    def endPos(self) -> tuple[int,int]:
+        return self.spaces[-1]
+
 class Piece:
     def __init__(self, board, color: str, loc: tuple[int,int]) -> None:
         self.board = board
@@ -5,28 +13,30 @@ class Piece:
         self.loc = loc
         self.hasMoved = False
     
-    def filterMoves(self, moves: list[tuple[int,int]]) -> list[tuple[int,int]]:
+    def filterMoves(self, moves: list[Move]) -> list[Move]:
         """Apply restrictions on moves that are common to all pieces, including staying on the board and not moving through or onto a blocked space"""
         filteredMoves = []
-        for move in moves:
-            if self.inBounds(move):
-                filteredMoves.append(move)
+        for candidate in moves:
+            if self.inBounds(candidate) and self.moveFree():
+                filteredMoves.append(candidate)
         return filteredMoves
 
-    def inBounds(self, loc: tuple[int]) -> bool:
-        return loc[0] >= 0 and loc[0] <= 7 and loc[1] >= 0 and loc[1] <= 7
+    def inBounds(self, mv: Move) -> bool:
+        """Return True if the last space of a move is on the board"""
+        pos = mv.endPos()
+        return pos[0] >= 0 and pos[0] <= 7 and pos[1] >= 0 and pos[1] <= 7
     
     def locBlocked(self, loc: tuple[int]) -> bool:
         """Return true if loc is occupied by a piece of the same color"""
 
 
 class King(Piece):
-    def moves(self) -> list[tuple[int]]:
+    def getMoves(self) -> list[Move]:
         moves = []
         for dy in range(-1,2):
             for dx in (-1,2):
                 if dy != 0 or dx != 0:
-                    moves.append((self.loc[0] + dy, self.loc[1] + dx))
+                    moves.append(Move((self.loc[0] + dy, self.loc[1] + dx)))
         return self.filterMoves(moves)
 
     def __str__(self) -> str:
