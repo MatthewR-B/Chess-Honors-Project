a50079afd456475586961bf55d870d185085195e
matthewrb <reedbrownm@gmail.com>
2024-09-05 21:59:05 -0400

    Add getSpace and setSpace methods for interfacing with the board with tuples

--- game.py
+++ game.py
@@ -21,11 +21,18 @@ class Game:
         return boardStr
 
     def move(self, pos1: tuple[int,int], pos2: tuple[int,int]) -> None:
-        piece = self.board[pos1[0]][pos1[1]]
-        piece.location = pos2
-        self.board[pos2[0]][pos2[1]] = piece
-        self.board[pos1[0]][pos1[1]] = None
+        piece = self.getSpace(pos1)
+        piece.loc = pos2
+        self.setSpace(piece,pos2)
+        self.setSpace(None,pos1)
         # Add move logging here
+
+    def getSpace(self, pos: tuple[int,int]) -> Piece | None:
+        return self.board[pos[0]][pos[1]]
+
+    def setSpace(self, content: Piece | None, pos: tuple[int,int]) -> None:
+        self.board[pos[0]][pos[1]] = content
+
     
 if __name__ == "__main__":
     g = Game()
