538e3aa2b86dd9ffd2bae5636be5be47ad7167ea
matthewrb <reedbrownm@gmail.com>
2024-09-05 22:13:14 -0400

    Add move testing loop with user input

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from game import Game
 
 g = Game()
 print(g)
+
+# while True:
+#     pos1 = input("Enter starting position: ")
+#     if (pos1 == "quit"): break
+#     pos2 = input("Enter ending position: ")
+#     if (pos2 == "quit"): break
+#     g.move(tuple(pos1.split(',')),tuple(pos2.split(',')))
