2bcd9d8470aa64175032dc3460f616c66e99e954
matthewrb <reedbrownm@gmail.com>
2024-10-24 18:08:12 -0400

    Refactor Game initialization

--- game.py
+++ game.py
@@ -2,25 +2,17 @@ from pieces import *
 from typing import Optional
 
 class Game:
+    def __init__(self, populate: bool = True) -> None:
         """Initialize board and populate with starting pieces"""
         self.board = [[None]*8 for i in range(8)]
         self.moveHistory = []
-        pieceList = [Rook,Knight,Bishop,Queen,King,Bishop,Knight,Rook]
-        col = 0
-        for piece in pieceList:
-            self.board[0][col] = piece(self,'black',(0,col))
-            self.board[1][col] = Pawn(self,'black',(1,col))
-            self.board[6][col] = Pawn(self,'white',(6,col))
-            self.board[7][col] = piece(self,'white',(7,col))
-            col += 1
-    
-    def __str__(self) -> str:
-        boardStr = ""
-        for row in self.board:
-            for piece in row:
-                boardStr += ('-' if piece == None else piece.__str__()) + ' '
-            boardStr += "\n"
-        return boardStr
+        if populate:
+            pieceList = [Rook,Knight,Bishop,Queen,King,Bishop,Knight,Rook]
+            for col in range(8):
+                self.setSpace(pieceList[col](self,'black',(0,col)), (0,col))
+                self.setSpace(Pawn(self,'black',(1,col)), (1,col))
+                self.setSpace(Pawn(self,'white',(6,col)), (6,col))
+                self.setSpace(pieceList[col](self,'white',(7,col)), (7,col))
 
     def move(self, mv: Move) -> None:
         piece = self.getSpace(mv.startPos())
